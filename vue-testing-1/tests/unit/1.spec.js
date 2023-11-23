14b50f25c565808137d8e81a0a6eebdcd5989a37
robmainah <44176348+robmainah@users.noreply.github.com>
2023-11-23 19:13:34 +0300

    test methods

--- vue-testing-1/tests/unit/1.spec.js
+++ vue-testing-1/tests/unit/1.spec.js
@@ -1,4 +1,5 @@
 import { mount } from "@vue/test-utils"
+import { nextTick } from "vue"
 
 const App = {
   data () {
@@ -6,7 +7,13 @@ const App = {
       count: 0
     }
   },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
   template: `
+    <button @click="increment"></button>
     <div v-if="count % 2 === 0">
       Count: {{ count }}. Count is even
     </div>
@@ -16,7 +23,7 @@ const App = {
   `
 }
 
-const factory = ({ data }) => {
+const factory = ({ data } = { data: {} }) => {
   return mount(App, {
     data() {
       return data
@@ -25,21 +32,17 @@ const factory = ({ data }) => {
 }
 
 describe('App', () => {
-  it('test for odd', () => {
-    const wrapper = factory({ 
-      data: {
-        count: 1
-      }
-    })
+  it('test for odd', async () => {
+    const wrapper = factory()
+    wrapper.find('button').trigger('click')
+    await nextTick()
     expect(wrapper.html()).toContain('Count: 1. Count is odd')
   })
 
-  it('test for even', () => {
-    const wrapper = factory({ 
-      data: {
-        count: 2
-      }
-    })
+  it('test for even', async () => {
+    const wrapper = factory()
+    await wrapper.find('button').trigger('click')
+    await wrapper.find('button').trigger('click')
     expect(wrapper.html()).toContain('Count: 2. Count is even')
   })
 })
