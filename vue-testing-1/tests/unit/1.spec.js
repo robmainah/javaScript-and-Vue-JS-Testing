5c9826f34743343dee8186e88395c09e92aa963a
robmainah <44176348+robmainah@users.noreply.github.com>
2023-11-23 18:14:25 +0300

    test passing props

--- vue-testing-1/tests/unit/1.spec.js
+++ vue-testing-1/tests/unit/1.spec.js
@@ -1,14 +1,35 @@
 import { mount } from "@vue/test-utils"
 
 const App = {
+  props: {
+    count: {
+      type: Number
+    }
+  },
   template: `
-    <div>Hello</div>
+    <div v-if="count % 2 === 0">
+      Count: {{ count }}. Count is even
+    </div>
+    <div v-else>
+      Count: {{ count }}. Count is odd
+    </div>
   `
 }
 
-test('App', () => {
-  const wrapper = mount(App)
-  // console.log(wrapper.html());
-  expect(wrapper.html()).toBe('<div>Hello</div>')
-})
+const factory = (props) => {
+  return mount(App, {
+    props
+  })
+}
 
+describe('App', () => {
+  it('test for odd', () => {
+    const wrapper = factory({ count: 1 })
+    expect(wrapper.html()).toContain('Count: 1. Count is odd')
+  })
+  
+  it('test for even', () => {
+    const wrapper = factory({ count: 2})
+    expect(wrapper.html()).toContain('Count: 2. Count is even')
+  })
+})
